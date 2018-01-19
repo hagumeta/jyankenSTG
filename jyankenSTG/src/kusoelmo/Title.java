8806f24c5061c44673bc0a2bd25454d2b8d54f94
裕人 原田 <e1431@s.akashi.ac.jp>
2018-01-19 16:48:04 +0900

    add new package kusoelmo

---
+++ jyankenSTG/src/kusoelmo/Title.java
@@ -0,0 +1,26 @@
+package kusoelmo;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Title implements KeyListener {
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
