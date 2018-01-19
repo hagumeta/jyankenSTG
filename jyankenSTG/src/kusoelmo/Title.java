1f26add41c833ec010548ac9171046a3587e1ab8
裕人 原田 <e1431@s.akashi.ac.jp>
2018-01-19 18:24:13 +0900

    Kusoelmo.Titleを追加(途中)

--- jyankenSTG/src/kusoelmo/Title.java
+++ jyankenSTG/src/kusoelmo/Title.java
@@ -3,24 +3,42 @@ package kusoelmo;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class Title implements KeyListener {
+	private int cursor;
+	private JPanel panel;
+	private JLabel label[];
+
+	public Title() {
+		cursor = 0;
+		panel = new JPanel();
+		label = new JLabel[4];
+		label[0] = new JLabel("CHALLENGE MODE (EASY)");
+		label[1] = new JLabel("CHALLENGE MODE (NORMAL)");
+		label[2] = new JLabel("CHALLENGE MODE (HARD)");
+		label[3] = new JLabel("ENDLESS MODE");
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		cursor++;
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		//使わない
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		//使わない
 	}
 
 }
